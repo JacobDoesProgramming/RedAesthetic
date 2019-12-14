e1c145b96fa8d3187976b3c6e8fed187867401b4
JacobDoesProgramming <58881499+JacobDoesProgramming@users.noreply.github.com>
2019-12-14 07:20:51 -0600

    JavaScript "Scroll To Top"
    If you are making a button off of this code, it must look like this: <button onclick="topfunction()" id="button" style="display: block;">Back To Top!</button>. The text between the two <button> tags is changeable, so is the id, but you will have to edit the javascript code too. If you're being really hardcore you can change the function name, but again, you have to change it in the javascript

---
+++ RedAesthetic.js
@@ -0,0 +1,16 @@
+
+        mybutton = document.getElementById("button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+function topfunction(){
+document.documentElement.scrollTop = 83;
+}
+    
